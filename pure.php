2e385627998bf281971b08359e7ed89afb7abdf7
Arshed Ahmed <arshedahmedmail@gmail.com>
2022-05-08 13:30:13 +0530

    create php file

---
+++ pure.php
@@ -0,0 +1,21 @@
+<?php
+
+$name1 = "Nimal";
+$name2 = "Kamal";
+
+echo("<h1>Hello World!</h1>");
+echo("<table border='1'>");
+    echo("<tr>");
+        echo("<td>Id</td>");
+        echo("<td>Name</td>");
+    echo("</tr>");
+    echo("<tr>");
+        echo("<td>1</td>");
+        echo("<td>$name1</td>");
+    echo("</tr>");
+    echo("<tr>");
+        echo("<td>2</td>");
+        echo("<td>$name2</td>");
+    echo("</tr>");
+echo("</table>");
+?>
