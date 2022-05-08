2c586e736c31a1540ec3aa2383b6f8d268d7e104
Arshed Ahmed <arshedahmedmail@gmail.com>
2022-05-08 15:46:07 +0530

    connect to mysql

---
+++ db.php
@@ -0,0 +1,15 @@
+<?php  
+$servername = "localhost";
+$username = "root";
+$password = "1234";
+$database = "earth";
+
+// create connection 
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// check connection 
+if(!$conn){
+    die("connection failed");
+}
+echo("connected succesfully");
+?>
